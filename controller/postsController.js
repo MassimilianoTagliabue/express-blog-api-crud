6728ad2427a228cea12ae630cfb0afef85b8c97c
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2024-12-04 19:26:45 +0100

    feat: cancellazione post

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,56 +1,69 @@
 const postList = require("../data/posts");
 
-const index = (req,res) => {
+const index = (req, res) => {
 
     res.json({
-        data : postList,
-        total : postList.length
+        data: postList,
+        total: postList.length
     });
 };
 
-const show = (req,res) =>{
+const show = (req, res) => {
 
     const postid = parseInt(req.params.id);
 
-   // res.json("leggiamo l'elemento con id " +postid );
-   const result = postList.find( curItem =>  curItem.id=== postid );
+    // res.json("leggiamo l'elemento con id " +postid );
+    const result = postList.find(curItem => curItem.id === postid);
 
-   console.log(result);
+    console.log(result);
 
-   if(result === undefined){
-    
-    res.sendStatus(404);
-   }else{
+    if (result === undefined) {
 
-    res.json(result);
-   }
+        res.sendStatus(404);
+    } else {
 
-   
+        res.json(result);
+    }
+
+
 };
 
-const create = (req,res) => {
+const create = (req, res) => {
 
     res.json("aggiungo un nuovo elemento");
 };
 
-const update = (req,res) =>{
+const update = (req, res) => {
 
     const postid = req.params.id;
-    res.json("modifico i dati dell'elemento con id " +postid);
+    res.json("modifico i dati dell'elemento con id " + postid);
 
 };
 
-const modify = (req,res) => {
+const modify = (req, res) => {
 
     const postid = req.params.id;
-    res.json("modifichiamo gli specifici dati dell'elemento con id " +postid);
+    res.json("modifichiamo gli specifici dati dell'elemento con id " + postid);
 };
 
-const destroy = (req,res) => {
+const destroy = (req, res) => {
 
     const postid = parseInt(req.params.id);
-    
-    res.json("elimino l'elemento con id " +postid);
+
+    const cancel = postList.findIndex((curItem) => curItem.id === postid);
+
+    if (cancel === -1) {
+        res.sendStatus(404);
+        
+    } else {
+        postList.splice(cancel, 1)
+        res.sendStatus(204);
+    }
+
+
+    console.log(postList);
+
+
 };
 
 
